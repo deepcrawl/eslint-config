89329b28d6a6269b5f972b6d46622b150927b11a
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-19 13:57:40 +0200

    Disable import/no-unresolved

--- index.js
+++ index.js
@@ -125,6 +125,7 @@ module.exports = {
     "import/no-named-as-default": "off",
     "import/no-named-as-default-member": "off",
     "import/no-relative-parent-imports": "error",
+    "import/no-unresolved": "off",
     "import/order": [
       "error",
       {
