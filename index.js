62e3844b0993e78d02fc27e99596f71ecc59fd3c
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-19 11:38:43 +0200

    feat: Add import/no-deprecated rule

--- index.js
+++ index.js
@@ -118,6 +118,7 @@ module.exports = {
     "array-func/avoid-reverse": "error",
     "clean-code/feature-envy": "error",
     "import/no-relative-parent-imports": "error",
+    "import/no-deprecated": "warn",
     "jest/expect-expect": "error",
     "no-loops/no-loops": "error",
     "prettier/prettier": [
