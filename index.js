db4c56f811ba23f668320bb55e99b9ae79038bf8
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-25 11:40:17 +0200

    Fix

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ module.exports = {
       },
       {
         selector: "variable",
-        format: ["camelCase", "UPPER_CASE"],
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
         leadingUnderscore: "allow",
         trailingUnderscore: "allow",
       },
