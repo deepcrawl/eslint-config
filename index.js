8ef4cf66e3bdbd209e601dc5662fab6df7d99fb6
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-05-25 08:10:28 +0200

    Update with new defaults

--- index.js
+++ index.js
@@ -26,7 +26,60 @@ module.exports = {
         default: "array-simple",
       },
     ],
-    "@typescript-eslint/ban-types": "error",
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        types: {
+          String: {
+            message: "Use string instead",
+            fixWith: "string",
+          },
+          Boolean: {
+            message: "Use boolean instead",
+            fixWith: "boolean",
+          },
+          Number: {
+            message: "Use number instead",
+            fixWith: "number",
+          },
+          Symbol: {
+            message: "Use symbol instead",
+            fixWith: "symbol",
+          },
+
+          Function: {
+            message: [
+              'The `Function` type accepts any function-like value.',
+              'It provides no type safety when calling the function, which can be a common source of bugs.',
+              'It also accepts things like class declarations, which will throw at runtime as they will not be called with `new`.',
+              'If you are expecting the function to accept certain arguments, you should explicitly define the function shape.',
+            ].join('\n'),
+          },
+
+          // object typing
+          Object: {
+            message: [
+              'The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.',
+              '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
+              '- If you want a type meaning "any value", you probably want `unknown` instead.',
+            ].join('\n'),
+          },
+          '{}': {
+            message: [
+              '`{}` actually means "any non-nullish value".',
+              '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
+              '- If you want a type meaning "any value", you probably want `unknown` instead.',
+            ].join('\n'),
+          },
+          object: {
+            message: [
+              'The `object` type is currently hard to use ([see this issue](https://github.com/microsoft/TypeScript/issues/21732)).',
+              'Consider using `Record<string, unknown>` instead, as it allows you to more easily inspect and use the keys.',
+            ].join('\n'),
+          },
+        },
+      },
+    ],
     "@typescript-eslint/consistent-type-assertions": [
       "error",
       {
@@ -51,6 +104,7 @@ module.exports = {
         },
       },
     ],
+    "@typescript-eslint/member-delimiter-style": "error",
     "@typescript-eslint/naming-convention": [
       "error",
       {
@@ -77,7 +131,6 @@ module.exports = {
         ignoreRestArgs: true,
       },
     ],
-    "@typescript-eslint/no-floating-promises": "error",
     "@typescript-eslint/no-inferrable-types": [
       "error",
       {
@@ -104,14 +157,14 @@ module.exports = {
         argsIgnorePattern: "^_",
       },
     ],
-    "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/no-useless-constructor": "error",
     "@typescript-eslint/prefer-for-of": "error",
     "@typescript-eslint/prefer-function-type": "error",
+    "@typescript-eslint/prefer-includes": "error",
     "@typescript-eslint/prefer-nullish-coalescing": "error",
     "@typescript-eslint/prefer-optional-chain": "error",
+    "@typescript-eslint/prefer-string-starts-ends-with": "error",
     "@typescript-eslint/require-array-sort-compare": "error",
-    "@typescript-eslint/restrict-plus-operands": "error",
     "@typescript-eslint/return-await": "error",
     "@typescript-eslint/unified-signatures": "error",
     "array-func/avoid-reverse": "error",
