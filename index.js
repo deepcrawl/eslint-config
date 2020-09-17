dce3a98dc5e8d9c6fadedfe496c4f24f791a8e7d
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-09-17 11:10:04 +0200

    feat: Change naming-convention severity to warn

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ module.exports = {
     ],
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/naming-convention": [
-      "error",
+      "warn",
       {
         selector: "default",
         format: ["camelCase"],
