dd4d6ae55323c40fd03493f2266fac3981a9a605
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2026-05-22 10:16:07 +0200

    revert: "fix: Restore intersection and union types sorting"
    This reverts commit ccdd8a3b57b7d06dd201928fd9d1c1a2ba0ed162.

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import jestPlugin from "eslint-plugin-jest";
 import nodePlugin from "eslint-plugin-n";
 import noLoopsPlugin from "eslint-plugin-no-loops";
 import packageJsonPlugin from "eslint-plugin-package-json";
-import perfectionist from "eslint-plugin-perfectionist"
 import promisePlugin from "eslint-plugin-promise";
 import sonarjsPlugin from "eslint-plugin-sonarjs";
 import typescriptEslint from "typescript-eslint";
@@ -173,7 +172,7 @@ export default defineConfig([
     },
   },
   {
-    plugins: { "@stylistic": stylisticPlugin, perfectionist },
+    plugins: { "@stylistic": stylisticPlugin },
     rules: {
       complexity: ["error", 15],
       "default-case-last": "error",
@@ -225,8 +224,6 @@ export default defineConfig([
       "no-self-assign": "error",
       "no-unreachable-loop": "error",
       "object-shorthand": "error",
-      "perfectionist/sort-intersection-types": "error",
-      "perfectionist/sort-union-types": "error",
       "prefer-template": "error",
       "promise/prefer-await-to-then": "error",
     },
